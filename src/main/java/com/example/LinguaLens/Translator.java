e97a84635d92668c39db118984b73b94ccf155f2
Sezsie <129116635+Sezsie@users.noreply.github.com>
2023-10-30 11:35:37 -0500

    preparatory changes for translator
    minor changes to the translator class, added variables for the api key and target url. also added additional comments into the translator class and removed the TODO.
    
    P.S: I will start actively developing the translator class today.

--- src/main/java/com/example/LinguaLens/Translator.java
+++ src/main/java/com/example/LinguaLens/Translator.java
@@ -1,11 +1,22 @@
 package com.example.LinguaLens;
 
-// This code reaches out to OpenAI's ChatGPT and asks it to return the translated text.
+// This code reaches out to OpenAI's ChatGPT and asks it to return the translated text. 
+// It then returns the translated text to the Reader class. 
 public class Translator {
     Utilities u = new Utilities();
-    //TODO: Actually write the translator class after the implementation of the Jackson project.
+    
+    // Constants to store the URL endpoint and our API key for OpenAI's ChatGPT.
+    private static final String TARGET_URL = "";
+    private static final String API_KEY = "";
+
     public String translate(String text, String language) {
         String translatedText = "";
+        
+        // arbitrary code to silence the warnings on target_url and api_key being unused for now.
+        if (TARGET_URL == "" && API_KEY == "") {
+            u.print("Silencing warnings");
+        }
+
         return translatedText;
     }
 
