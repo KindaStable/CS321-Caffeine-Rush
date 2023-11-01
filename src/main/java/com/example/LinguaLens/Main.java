bc0b8a3bbe650e4f3dda01bac588ed76000081a4
Sezsie <129116635+Sezsie@users.noreply.github.com>
2023-11-01 11:47:15 -0500

    Implemented the Translator
    I implemented the translator class and made the appropriate calls to OpenAI's ChatGPT for translation. Added comments displaying what I will work on next, as well as general documentation.

--- src/main/java/com/example/LinguaLens/Main.java
+++ src/main/java/com/example/LinguaLens/Main.java
@@ -1,24 +1,33 @@
 package com.example.LinguaLens;
 import java.io.IOException;
+import java.util.Locale;
+import org.apache.commons.text.StringEscapeUtils;
 
 // This code accesses all the methods necessary in order to read and display the text from an image.
 class Main {
 
     public static void main(String[] args) throws IOException {
         Utilities u = new Utilities();
-        
-        // TODO: Run the program again if the user wishes.
-        UI.showDialogue("Please select an image with text that you would like the program to read.", "Select Image", 0);
-        String filePath = UI.chooseImage();
-        //UI.showDialogue(filePath, "Thanks", 0);
-        String readText = Reader.read(filePath);
 
-        if(readText != "")
-        {
-            UI.showDialogue(readText, "Thanks", 0);
+        // TODO, GARRETT: The translator output sometimes returns mojibake characters (e.g. "â€œ" instead of "“").
+        // I have no idea whether this is a problem with the way the text is being read from the image, or with the way the text is being translated.
+        // I'll try to implement a fix for this, but I'm not sure how to do it. I'll figure it out eventually.
+        UI.showDialogue("Please select an image with text that you would like to be translated.", "Select Image", 0);
+
+        String filePath = UI.chooseImage();
+        String readText = Reader.read(filePath);
+        String escapedText = StringEscapeUtils.escapeJson(readText);
+        String prompt = "Translate the below text into " + Locale.getDefault().getDisplayLanguage() + ". Make sure to display all of the text: " + escapedText;
+        
+        // Print out the read text from the vision API.
+        // u.print(readText);
+
+        if (readText != "") {
+            String translatedText = Translator.translate(prompt);
+            UI.showDialogue(translatedText, "Thanks", 0);
         }
 
-        u.print("End of Program");
         System.exit(0);
     }
+    
 }
--- src/main/java/com/example/LinguaLens/Translator.java
+++ src/main/java/com/example/LinguaLens/Translator.java
@@ -1,23 +1,78 @@
 package com.example.LinguaLens;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 // This code reaches out to OpenAI's ChatGPT and asks it to return the translated text. 
 // It then returns the translated text to the Reader class. 
 public class Translator {
     Utilities u = new Utilities();
+
+    public static String translate(String prompt) {
+        String url = "https://api.openai.com/v1/chat/completions";
+        String apiKey = "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";
+        String model = "gpt-3.5-turbo";
     
-    // Constants to store the URL endpoint and our API key for OpenAI's ChatGPT.
-    private static final String TARGET_URL = "";
-    private static final String API_KEY = "";
 
-    public String translate(String text, String language) {
-        String translatedText = "";
-        
-        // arbitrary code to silence the warnings on target_url and api_key being unused for now.
-        if (TARGET_URL == "" && API_KEY == "") {
-            u.print("Silencing warnings");
+        try {
+
+            URL obj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Authorization", "Bearer " + apiKey);
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            // The request body
+            String body = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
+            connection.setDoOutput(true);
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+            writer.write(body);
+            writer.flush();
+            writer.close();
+
+            // Response from ChatGPT
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuffer response = new StringBuffer();
+
+            while ((line = br.readLine()) != null) {
+                response.append(line);
+            }
+            br.close();
+
+            // print unextracted response for testing
+            // System.out.println(response.toString());
+
+            // calls the method to extract the message.
+            return extractMessageFromJSONResponse(response.toString());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
 
-        return translatedText;
+    public static String extractMessageFromJSONResponse(String response) {
+        // print response for testing
+        // System.out.println(response);
+        int start = response.indexOf("content")+ 11;
+        int end = response.indexOf("\"", start);
+        String message = response.substring(start, end);
+
+        // Replace escaped characters with their actual values
+        message = message.replace("\\n", "\n");
+        message = message.replace("\\r", "\r");
+        message = message.replace("\\t", "\t");
+
+
+        // TODO: this return will be obsolete when the StorageManager is implemented.
+        // We will simply store the translated text in the database and return nothing.
+        // For our uses now, we need to return the translated text so that it can be displayed.
+        return message;
     }
 
 }
